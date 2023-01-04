1afea5379ac39e71305028a1a1c9f880bb02880d
Kruutteri1 <97964873+Kruutteri1@users.noreply.github.com>
2023-01-04 19:47:06 +0200

    Add files via upload

---
+++ c++_labs/Ex11-Lab03.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+
+// Âàð³àíò 11 Ï³äâåñòè ÷èñëî n ó ñòóï³íü m.
+int main() {
+        double n, m, res;
+
+
+        std::cout << "Enter n:";
+        std::cin >> n;
+        std::cout << "Enter m:";
+        std::cin >> m;
+        
+        res = pow(n, m);
+        std::cout << res;
+}
